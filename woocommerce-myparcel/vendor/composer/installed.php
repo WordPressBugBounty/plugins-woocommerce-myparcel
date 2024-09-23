16e089673be7e634263620eed5b19a4087937f80
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 01:25:47 +0000

    4.24.0

---
+++ woocommerce-myparcel/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'myparcelnl/woocommerce',
+        'pretty_version' => '4.24.0',
+        'version' => '4.24.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myparcelnl/sdk' => array(
+            'pretty_version' => '7.16.0',
+            'version' => '7.16.0.0',
+            'reference' => '44ccb0c86c33dc568edda2ed47b19cfae23f91a9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../myparcelnl/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'myparcelnl/woocommerce' => array(
+            'pretty_version' => '4.24.0',
+            'version' => '4.24.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
